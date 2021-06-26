353b52719934bf09457da5f60ea169e5f1bf4510
Felipe Marinho <fmarinho05@hotmail.com>
2021-06-25 23:03:23 -0300

    Média com estrutura While

--- Media While.py
+++ Media While.py
@@ -12,16 +12,20 @@
 #     contador = contador + 1
 
 
-
+# Serve para guarda a qntd de notas que eu quero colocar.
 quantidadeDeNotas = int(input("Quantas notas você gostaria de registrar ? "))
 #quantidadeDeNotas = int(quantidadeDeNotas)
 
+# Contar a qntd as vezes que a repetição já aconteceu.
 contador = 0
+
+# Serve para guardar as somas das notas fornecidas.
 acumulador = 0
 
 while(contador < quantidadeDeNotas):
-    print(contador,acumulador)
-    acumulador = acumulador + 70
-    contador = contador + 1
+   nota = int(input("Insira a nota " + str(contador) + ": "))
+   #nota = int(nota)
+   acumulador = acumulador + nota
+   contador = contador + 1
 
 print(acumulador / quantidadeDeNotas)
