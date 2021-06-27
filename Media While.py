ee7669266706b5821e8bcb7f20ed3729623ef8ae
Felipe Marinho <fmarinho05@hotmail.com>
2021-06-27 10:10:29 -0300

    Correção

--- Media While.py
+++ Media While.py
@@ -13,18 +13,20 @@
 
 
 # Serve para guarda a qntd de notas que eu quero colocar.
+
 quantidadeDeNotas = int(input("Quantas notas você gostaria de registrar ? "))
 #quantidadeDeNotas = int(quantidadeDeNotas)
 
-# Contar a qntd as vezes que a repetição já aconteceu.
+# Serve para contar a qntd de vezes que a repetição vai acontecer.
 contador = 0
 
 # Serve para guardar as somas das notas fornecidas.
 acumulador = 0
 
 while(contador < quantidadeDeNotas):
-   nota = int(input("Insira a nota " + str(contador) + ": "))
+   nota = int(input("Insira a nota " + str(contador + 1) + ": "))
    #nota = int(nota)
+
    acumulador = acumulador + nota
    contador = contador + 1
 
