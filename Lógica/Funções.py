991b01d1f72e039fc53a1b9f6eb520d2a61c1a5a
Felipe Marinho <fmarinho05@hotmail.com>
2021-07-16 16:08:43 -0300

    Funções

--- Lógica/Funções.py
+++ Lógica/Funções.py
@@ -1,4 +1,4 @@
-#def - Serve para definir funções onde terá uma sequencia de comandos, e quando você precisar dessas sequência em alguma parte do programa basta chama-la.
+#def - Serve para criar funções onde terá uma sequencia de comandos, e quando você precisar dessas sequência em alguma parte do programa basta chama-la.
 
 #Adicionando mais 1
 def incremento(numero):
@@ -25,14 +25,40 @@ def multiplicacao(primeiro, segundo):
 
 print(multiplicacao(10, 10))
 
-#Divisão
+#Divisão - Realiza a divisão e dá o resultado baseado na razão da operação, ou seja, será retornado um valor com casas decimais.
 def divisao(um, dois):
     return um / dois
 
 print(divisao(20, 20))
 
-#Potencia elevada
-def expo(base, expoente):
-    return base ** expoente
+#MOD - É um operador aritmético que é responsável por retornar o resto de uma divisão.
+def resto(a, b):
+    return a % b
 
-print(expo(5,1))
+print(resto(14,4))
+
+#DIV - Realiza a divisão e dá o resultado baseado no quociente.
+def quociente(a, b):
+    return a // b
+
+print(quociente(14, 4))
+
+#Potencia elevada
+# def expo(base, expoente):
+#     return base ** expoente
+
+# print(expo(5,10))
+
+def expo(base, expoente):
+    if type(base) == type("") or type(expoente) == type(""):
+        print("Você precisa digitar um número, por favor! ")
+    else:
+        return base ** expoente
+
+print(expo(5,8))
+
+b = (int(input("Digite a base da operação: ")))
+
+e = (int(input("Digite o expoente da operação: ")))
+
+print(expo(b, e))
